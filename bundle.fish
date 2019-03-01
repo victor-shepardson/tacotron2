ba9b938fe949d6279744f2292d76829794de2fcc
Victor Shepardson <victor.shepardson@gmail.com>
2019-03-01 13:14:20 -0500

    more poking around in inference nb

--- bundle.fish
+++ bundle.fish
@@ -1,7 +1,5 @@
 #set pkg_dir (dirname (which python))/../lib/python3.7/site-packages/
-
 #set tf_contrib_sos (string replace -r $pkg_dir\/ '' -- (find $pkg_dir/tensorflow/contrib -name '*.so'))
-
 #pyinstaller inference.py -y -p "./waveglow" \
 #    --add-data "*.pt:." --hidden-import sklearn.neighbors.typedefs \
 #    --hidden-import sklearn.neighbors.quad_tree --hidden-import sklearn.tree --hidden-import sklearn.tree._utils \
