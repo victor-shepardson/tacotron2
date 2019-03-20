fb7f70ab71d4f301b0bad095cf66c17c405311eb
Victor Shepardson <victor.shepardson@gmail.com>
2019-03-20 11:31:56 -0400

    move data copy to collate

--- data_utils.py
+++ data_utils.py
@@ -112,3 +112,53 @@ class TextMelCollate():
 
         return text_padded, input_lengths, mel_padded, gate_padded, \
             output_lengths
+
+class MelCollate():
+    # TODO: remove text processing
+    """ Zero-pads model inputs and targets based on number of frames per setep
+    """
+    def __init__(self, n_frames_per_step, model):
+        self.n_frames_per_step = n_frames_per_step
+        self.model = model
+
+    def __call__(self, batch):
+        """Collate's training batch from normalized text and mel-spectrogram
+        PARAMS
+        ------
+        batch: [text_normalized, mel_normalized]
+        """
+        # Right zero-pad all one-hot text sequences to max input length
+        input_lengths, ids_sorted_decreasing = torch.sort(
+            torch.LongTensor([len(x[0]) for x in batch]),
+            dim=0, descending=True)
+        max_input_len = input_lengths[0]
+
+        text_padded = torch.LongTensor(len(batch), max_input_len)
+        text_padded.zero_()
+        for i in range(len(ids_sorted_decreasing)):
+            text = batch[ids_sorted_decreasing[i]][0]
+            text_padded[i, :text.size(0)] = text
+
+        # Right zero-pad mel-spec
+        num_mels = batch[0][1].size(0)
+        max_target_len = max([x[1].size(1) for x in batch])
+        if max_target_len % self.n_frames_per_step != 0:
+            max_target_len += self.n_frames_per_step - max_target_len % self.n_frames_per_step
+            assert max_target_len % self.n_frames_per_step == 0
+
+        # include mel padded and gate padded
+        mel_padded = torch.FloatTensor(len(batch), num_mels, max_target_len)
+        mel_padded.zero_()
+        gate_padded = torch.FloatTensor(len(batch), max_target_len)
+        gate_padded.zero_()
+        output_lengths = torch.LongTensor(len(batch))
+        for i in range(len(ids_sorted_decreasing)):
+            mel = batch[ids_sorted_decreasing[i]][1]
+            mel_padded[i, :, :mel.size(1)] = mel
+            gate_padded[i, mel.size(1)-1:] = 1
+            output_lengths[i] = mel.size(1)
+
+        # return text_padded, input_lengths, mel_padded, gate_padded, \
+            # output_lengths
+        return self.model.parse_batch((
+            text_padded, input_lengths, mel_padded, gate_padded, output_lengths))
--- train_uncond.py
+++ train_uncond.py
@@ -2,7 +2,7 @@
 
 # python train_uncond.py -o ./checkpoints -l ./logs --n_gpus 0 --hparams "training_files='filelists/mcv_eo_train_filelist.txt',validation_files='filelists/mcv_eo_val_filelist.txt',batch_size=1,iters_per_checkpoint=5"
 
-# TODO: discard overlong and low power examples; smaller valid set; stratified samples by length; preconvert to wav; num_workers/pin_memory?
+# TODO: discard overlong and low power examples; smaller valid set; stratified samples by length; preconvert to spect; move async device copy to collate?
 
 import os
 import time
@@ -19,7 +19,7 @@ from torch.utils.data import DataLoader
 from fp16_optimizer import FP16_Optimizer
 
 from model_uncond import Tacotron2
-from data_utils import TextMelLoader, TextMelCollate
+from data_utils import TextMelLoader, MelCollate
 from loss_function import Tacotron2Loss
 from logger import Tacotron2Logger
 from hparams import create_hparams
@@ -56,11 +56,11 @@ def init_distributed(hparams, n_gpus, rank, group_name):
     print("Done initializing distributed")
 
 
-def prepare_dataloaders(hparams):
+def prepare_dataloaders(hparams, model):
     # Get data, data loaders and collate function ready
     trainset = TextMelLoader(hparams.training_files, hparams)
     valset = TextMelLoader(hparams.validation_files, hparams)
-    collate_fn = TextMelCollate(hparams.n_frames_per_step)
+    collate_fn = MelCollate(hparams.n_frames_per_step, model)
 
     train_sampler = DistributedSampler(trainset) \
         if hparams.distributed_run else None
@@ -151,7 +151,8 @@ def validate(model, criterion, valset, iteration, batch_size, n_gpus,
 
         val_loss = 0.0
         for i, batch in enumerate(val_loader):
-            x, y = model.parse_batch(batch)
+            # x, y = model.parse_batch(batch)
+            x, y = batch
             y_pred = model(x)
             loss = criterion(y_pred, y)
             if distributed_run:
@@ -202,7 +203,7 @@ def train(output_directory, log_directory, checkpoint_path, warm_start, n_gpus,
     logger = prepare_directories_and_logger(
         output_directory, log_directory, rank)
 
-    train_loader, valset, collate_fn = prepare_dataloaders(hparams)
+    train_loader, valset, collate_fn = prepare_dataloaders(hparams, model)
 
     # Load checkpoint if one exists
     iteration = 0
@@ -228,7 +229,8 @@ def train(output_directory, log_directory, checkpoint_path, warm_start, n_gpus,
                 param_group['lr'] = learning_rate
 
             model.zero_grad()
-            x, y = model.parse_batch(batch)
+            # x, y = model.parse_batch(batch)
+            x, y = batch
             y_pred = model(x)
 
             loss = criterion(y_pred, y)
