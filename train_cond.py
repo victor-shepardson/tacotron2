39766c83d71c3bb924c3be08184a89472ae03b1b
Victor Shepardson <victor.shepardson@gmail.com>
2019-04-17 17:17:54 -0400

    debug training

--- train_cond.py
+++ train_cond.py
@@ -160,6 +160,12 @@ def validate(model, criterion, valset, iteration, batch_size, n_gpus,
         val_loss = 0.0
         for i, batch in enumerate(val_loader):
             x, y = model.parse_batch(batch)
+
+            #debug
+            print('validating')
+            text_padded, input_lengths, mel_padded, max_len, output_lengths, speaker, language = x
+            print(input_lengths, max_len, text_padded.shape)
+
             y_pred = model(x)
             loss, mel_loss, gate_loss = criterion(y_pred, y, return_parts=True)
             if distributed_run:
