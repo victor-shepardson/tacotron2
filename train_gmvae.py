26aefd87414336acb761cf6430502089180f1397
Victor Shepardson <victor.shepardson@gmail.com>
2019-07-14 21:26:17 -0400

    auto name checkpoint dir by rank

--- train_gmvae.py
+++ train_gmvae.py
@@ -331,6 +331,9 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true',
                         help='print grad statistics')
 
+    if args.rank > 0:
+        args.checkpoint_path += '.' + str(args.rank)
+
     args = parser.parse_args()
     hparams = create_hparams(args.hparams)
     hparams.gpu = torch.cuda.is_available()
